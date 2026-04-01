3819522d6aa02e8a6c3ab7a5c994b740fdcc0503
praveencoder-157 <praveen95sundar@gmail.com>
2026-04-01 23:51:20 +0530

    Update server.js
    socket

--- server.js
+++ server.js
@@ -40,3 +40,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+io.on("connection", (socket) => {
+    console.log("🟢 User connected:", socket.id);
+});
